4e06fd457c5a64fd8fc0407904cbc1a0d629aba5
saneki <s@neki.me>
2022-02-23 18:30:55 -0600

    web: Use InterVariable as main font, specify fallback font

--- web/app/src/index.tsx
+++ web/app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '@fontsource/inter'
+import '@fontsource/inter/variable.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
